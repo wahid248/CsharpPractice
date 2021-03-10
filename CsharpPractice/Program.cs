c6029e31eef47abe180fb0e2ca8dc734fdafb9f1
Wahid-Laptop <wahid.cseaust@gmail.com>
2021-03-10 17:08:56 +0600

    linq demo

--- CsharpPractice/Program.cs
+++ CsharpPractice/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CsharpPractice
 {
@@ -6,7 +8,50 @@ namespace CsharpPractice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var cars = GetCars();
+            var customers = GetCustomers();
+            var customerToCar = GetToCustomerToCarMapping();
+
+            var ownedCars = cars.Where(c => customerToCar.Values.Contains(c.Key)).ToList();
+
+            foreach(var car in ownedCars)
+            {
+                Console.WriteLine(car.Value);
+            }
+        }
+
+        public static Dictionary<int, string> GetCustomers()
+        {
+            return new Dictionary<int, string>
+            {
+                { 1, "Jack" },
+                { 2, "Daniel" },
+                { 3, "Max" },
+                { 4, "Howard" },
+                { 5, "Stark" }
+            };
+        }
+
+        public static Dictionary<int, string> GetCars()
+        {
+            return new Dictionary<int, string>
+            {
+                { 1, "Marcedes" },
+                { 2, "Toyota" },
+                { 3, "Vox Wagon" },
+                { 4, "Tesla" },
+                { 5, "Corvette" }
+            };
+        }
+
+        public static Dictionary<int, int> GetToCustomerToCarMapping()
+        {
+            return new Dictionary<int, int>
+            {
+                { 1, 2 },
+                { 4, 3 },
+                { 5, 4 }
+            };
         }
     }
 }
